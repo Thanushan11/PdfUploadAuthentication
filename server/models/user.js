3f71396b115cdc8ad4b1442db582f8b936666a7b
Thanushan11 <ravithanu99@gmail.com>
2024-06-13 19:55:11 +0530

    “auth_completed”

--- server/models/user.js
+++ server/models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require ('mongoose')
 const jwt = require ('jsonwebtoken')
-const Joi = require("joi")
-const passwordcomplexity = require("joi-password-complexity")
+const Joi = require('joi');
+const passwordComplexity = require('joi-password-complexity');
 
 const userSchema = new mongoose.Schema({
      name:{
